786bc1774f2547e9934dc7e4f3fad85c53fc3040
John T. Wodder II <jwodder@sdf.lonestar.org>
2016-05-01 15:33:59 +0000

    Revert "Open logfile with the locale's preferred encoding"
    This reverts commit 8324088e175b2160b0db60796fb73f110aa334f3.
    
    Command-line arguments in Python 2 do not mix with filehandles opened with
    `io.open(encoding=?)`

--- daemail.py
+++ daemail.py
@@ -7,7 +7,6 @@ from   email.message          import Message
 from   email.mime.application import MIMEApplication
 from   email.mime.multipart   import MIMEMultipart
 from   email.mime.text        import MIMEText
-import io
 import locale
 import os
 import platform
@@ -179,7 +178,8 @@ def main():
         if args.logfile:
             # If no logfile was specified or this open() fails, die alone where
             # no one will ever know.
-            sys.stderr = io.open(args.logfile, 'a', encoding=prefenc)
+            sys.stderr = open(args.logfile, 'a')
+                ### What encoding do I use for this???
             print(datetime.now().isoformat(), errhead, file=sys.stderr)
             print('Command:', [args.command] + args.args, file=sys.stderr)
             print('From address:', args.sender, file=sys.stderr)
