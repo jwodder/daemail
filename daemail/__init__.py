ec79f044f7e076e4559f5215740b8c5bb56097bd
John T. Wodder II <git@varonathe.org>
2016-07-29 15:59:13 +0000

    Added `__author__` etc. variables

--- daemail/__init__.py
+++ daemail/__init__.py
@@ -1,7 +1,11 @@
 from   __future__ import unicode_literals
 import platform
 
-__version__ = '0.3.0.dev1'
+__version__      = '0.3.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'daemail@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/daemail'
 
 USER_AGENT = 'daemail {} ({} {})'.format(
     __version__, platform.python_implementation(), platform.python_version()
