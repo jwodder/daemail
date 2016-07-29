43c7be216422e66a6789ea115995373cfb017c65
John T. Wodder II <git@varonathe.org>
2016-07-29 15:55:50 +0000

    Mark current code as a dev version

--- daemail/__init__.py
+++ daemail/__init__.py
@@ -1,7 +1,7 @@
 from   __future__ import unicode_literals
 import platform
 
-__version__ = '0.3.0'
+__version__ = '0.3.0.dev1'
 
 USER_AGENT = 'daemail {} ({} {})'.format(
     __version__, platform.python_implementation(), platform.python_version()
