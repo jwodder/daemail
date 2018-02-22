3c6d2425342f588f2735c20c861ae01519ab9d09
John T. Wodder II <git@varonathe.org>
2018-02-22 23:50:21 +0000

    Typo

--- test/test_message.py
+++ test/test_message.py
@@ -14,7 +14,7 @@ def test_quopri_text():
 def test_quopri_multipart():
     msg = DraftMessage()
     msg.addtext(TEXT)
-    msg.addmimeblob(r'\0\0\0\0', 'application/octet-stream', 'null.dat')
+    msg.addmimeblob(b'\0\0\0\0', 'application/octet-stream', 'null.dat')
     blob = msg.compile()
     assert isinstance(blob, bytes)
     assert TEXT.encode('utf-8') not in blob
